2c59da4379c76d52775526d4057b0a843f08a23b
AkashSingh1141144 <AkashSingh.Webdeveloper@gmail.com>
2024-02-21 23:17:29 +0530

    Assignment

---
+++ Assignments/Variable-DataType/01.cpp
@@ -0,0 +1,15 @@
+// Take 2 interger values in two variables x and y and print their product.
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x =2;
+	int y =4;
+
+    int product = x * y; 
+	cout << "The Product of: " << x << " and " << y << " is "	<< product << endl;
+
+	return 0;
+}
